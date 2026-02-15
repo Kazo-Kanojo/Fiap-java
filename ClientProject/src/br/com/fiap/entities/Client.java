a4e254ed71a388f377a6a007a040737ca53e09cd
Enzo <enzopyton24@gmail.com>
2026-02-15 00:13:04 -0300

    Iniciando meu estudos em java + desafio proposto em sala realizado

---
+++ ClientProject/src/br/com/fiap/entities/Client.java
@@ -0,0 +1,43 @@
+package br.com.fiap.entities;
+
+public class Client {
+    private String nome;
+    private String cpf;
+    private int idade;
+    private double altura;
+
+    // Metodos seteers
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    //metodos getters
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public double getAltura(){
+        return altura;
+    }
+}
+
+
